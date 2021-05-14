4feda44fce0b83cb3ecfba11c596fd7b52c6877d
YoonJam <dbsgusdn111@naver.com>
2021-05-14 20:35:49 +0900

    add - libft.h , 헤더 적용

--- libft/ft_strlcpy.c
+++ libft/ft_strlcpy.c
@@ -5,9 +5,24 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: hyyoon <hyyoon@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/05/14 18:03:04 by hyyoon            #+#    #+#             */
-/*   Updated: 2021/05/14 18:03:06 by hyyoon           ###   ########.fr       */
+/*   Created: 2021/05/14 19:11:59 by hyyoon            #+#    #+#             */
+/*   Updated: 2021/05/14 19:12:01 by hyyoon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 
+size_t  strlcpy(char *dest, const char* src, size_t dstsize)
+{
+    size_t length;
+
+    length = 0;
+    while (*src != '\0' && length < dstsize - 1)
+    {
+        *dest++ = *src++;
+        length++;
+    }
+    *dest = '\0';
+
+    return length;
+}
