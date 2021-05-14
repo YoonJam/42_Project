e2901077926a802f9a75227dbc645c74b8ea04cd
YoonJam <dbsgusdn111@naver.com>
2021-05-14 18:02:03 +0900

    add - ft_atoi

---
+++ libft/ft_atoi.c
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hyyoon <hyyoon@student.42seoul.kr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/05/14 17:41:04 by hyyoon            #+#    #+#             */
+/*   Updated: 2021/05/14 17:41:18 by hyyoon           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int is_space(int c)
+{
+    if ((c >= 9 && c <= 13) || c == 32)
+        return (1);
+    return(0);
+}
+
+int is_num(int c)
+{
+    if (c >= '0' && c <= '9')
+        return (1);
+    return(0);
+}
+
+int ft_atoi(const char *str)
+{
+    int num;
+    int sign;
+
+    num = 0;
+    sign = 1;
+    while (is_space(*str))
+        str++;
+    while (*str == '-' && *str == '+')
+    {
+        if (*str == '-')
+            sign *= -1;
+        str++;
+    }
+    while (is_num(*str))
+    {
+        num *= 10;
+        num += (*str - '0');
+        str++;
+    }
+    
+    return (sign * num);
+}
