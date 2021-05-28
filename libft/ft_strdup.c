aee5b6d2fbceef772f930beef33faaef27250f87
YoonJam <dbsgusdn111@naver.com>
2021-05-28 20:12:10 +0900

    update calloc, memcpy - finish  ,strdup - 1 waring

--- libft/ft_strdup.c
+++ libft/ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: hyyoon <hyyoon@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/24 16:31:56 by hyyoon            #+#    #+#             */
-/*   Updated: 2021/05/24 17:27:30 by hyyoon           ###   ########.fr       */
+/*   Updated: 2021/05/28 20:07:41 by hyyoon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,17 @@
 
 char	*ft_strdup(const char *s1)
 {
-	char* str;
-	str = (char*)s1;
-	
-	return (str);
+	char* ret;
+	size_t len; 
+
+	len = ft_strlen(s1);
+	ret = (char*)malloc(sizeof(char*) * (len + 1));
+	if (ret != NULL)
+	{
+		while (*s1 != '\0')
+			*ret++ = *s1++;
+		*ret = '\0';
+		ret -= len;
+	}
+	return ret;
 }
