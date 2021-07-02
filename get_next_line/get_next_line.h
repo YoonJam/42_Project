c50792581f1d7375560b17af3608ddcb079dee3a
YoonJam <dbsgusdn111@naver.com>
2021-07-02 18:21:21 +0900

    init - get_next_line

--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -6,7 +6,17 @@
 /*   By: hyyoon <hyyoon@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/16 15:36:18 by hyyoon            #+#    #+#             */
-/*   Updated: 2021/06/16 15:39:22 by hyyoon           ###   ########.fr       */
+/*   Updated: 2021/07/02 15:32:06 by hyyoon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+
+int get_next_line(int fd, char **line);
+
+#endif
